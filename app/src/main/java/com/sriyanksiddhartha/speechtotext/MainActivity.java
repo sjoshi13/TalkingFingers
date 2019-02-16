22f7b3e422a46a89d6abf374b75889571f0ac8b7
sjoshi13 <shruti03.joshi@gmail.com>
2019-02-16 22:16:50 +0530

    videos playabck

--- app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/speechtotext/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sriyanksiddhartha.speechtotext;
 
 import android.content.Intent;
+import android.graphics.Path;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
@@ -12,7 +14,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
-
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		output_video = (VideoView) findViewById(R.id.videoView);
 	}
 public void getTextInput(){
+
 		String input = text_input.getText().toString().trim();
 	if(TextUtils.isEmpty(input)){
 		Toast.makeText(this,"Enter email", Toast.LENGTH_SHORT).show();
@@ -41,18 +43,41 @@ public void getTextInput(){
 
 }
 	public void onClick(View v) {
-		String input = text_input.getText().toString().trim();
-		String videopath = String.format("android.resource://com.sriyanksiddhartha.speechtotext/\\%s\\%s",input);
-		String vid="android.resource://com.sriyanksiddhartha.speechtotext/"+R.raw.baby;
-		System.out.println(videopath);
-		System.out.println(vid);
-		Uri uri= Uri.parse(vid);
-		output_video.setVideoURI(uri);
-		output_video.start();
 		if(v==get_text_input)
 		{
 			txvResult.setText(text_input.getText().toString().trim());
 		}
+		String inputt = text_input.getText().toString().trim();
+		String tokens[] = inputt.split(" ");
+
+		//String videopath = String.format("android.resource://com.sriyanksiddhartha.speechtotext/R.raw.%s",input);
+
+		//String vid="android.resource://com.sriyanksiddhartha.speechtotext/"+R.raw.baby;
+		//System.out.println(videopath);
+		//System.out.println(vid);
+
+int i=0;
+
+        for(String input : tokens) {
+        	System.out.print("here");
+			//  String str = "countdown"; //Suppose input word is "countdown"
+			int videoResource = getResources().getIdentifier(input, "raw", getPackageName()); //calculating id for that video name
+			String path = "android.resource://" + getPackageName() + "/" + videoResource; //using the id found above for the required video
+			Uri uri = Uri.parse(path);
+			output_video.setVideoURI(uri);
+			output_video.start();
+            output_video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+				@Override
+				public void onCompletion(MediaPlayer mediaPlayer) {
+					Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+
+				}
+			});
+
+
+
+		}
+
 		}
 	public void getSpeechInput(View view) {
 
